9f6c0abf0fb15a2e4f01bfe310c9d03d7942de8d
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2022-03-15 19:23:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    var typed = new Typed(".typing", {
+        strings: ["STUDENT.", "DEVELOPER.", "ENTHUSIAST.", "FREELANCER."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
